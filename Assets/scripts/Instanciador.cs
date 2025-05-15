125784a86eb96a8f6d8edcc38d13066fa04422b3
Jonas "El  pipa" LECHERSON <49516747@est.ort.edu.ar>
2025-05-15 10:45:11 -0300

    cosas

--- Assets/scripts/Instanciador.cs
+++ Assets/scripts/Instanciador.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movimento : MonoBehaviour
+public class Instanciador : MonoBehaviour
 {
+    public GameObject Prefab1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawneaprefab();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    public void spawneaprefab()
+    {
+        Instantiate(Prefab1);
+    }
+
 }
---
+++ Assets/scripts/Movimento.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    public float velocidad = 5f;
+    public float fuerzaSalto = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+        Vector3 direccion = new Vector3(HorizontalInput, 0, VerticalInput);
+        transform.position += direccion * velocidad * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+        }
+    }
+}
